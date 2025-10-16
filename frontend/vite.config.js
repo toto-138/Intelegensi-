4d7ba9b29f5325dc42b32ea2690d0d73fb85ddf6
toto-138 <bayandika39@gmail.com>
2025-10-17 03:02:52 +0700

    Delete frontend/vite.config.js

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Ganti `username` dengan username GitHub kamu
-// Ganti `frontend` dengan nama repo kamu
-export default defineConfig({
-  plugins: [react()],
-  base: "/frontend/",
-});
