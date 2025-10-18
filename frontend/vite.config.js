c34abe066706b1027fc93f9a46a7b2319c62ed73
toto-138 <bayandika39@gmail.com>
2025-10-19 01:38:07 +0700

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// ganti "Intelegensi-" sesuai nama repo GitHub kamu
-export default defineConfig({
-  plugins: [react()],
-  base: "/Intelegensi-/"
-});
